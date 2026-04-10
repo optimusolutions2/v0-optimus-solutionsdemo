95777776ba642a2abcac6ad8d0b0dff91718fa5a
optimusolutions2 <optimusolutions2@gmail.com>
2026-04-10 23:38:24 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-sans antialiased`}>
-        {children}
+      <body className={`${inter.variable} font-sans antialiased`}>  {children}
         <Analytics />
+  {children}
+  <WhatsAppButton />
       </body>
     </html>
   )
